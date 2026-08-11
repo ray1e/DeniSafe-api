f2826aefeab3cf70c076ed82d9e0bfc1bb22d2b5
collins kimani <qeshyy@gmail.com>
2026-08-11 12:28:53 +0300

    refactor(backend): move item delete logic from controller to service

--- Backend/services/debts.service.js
+++ Backend/services/debts.service.js
@@ -32,10 +32,7 @@ export const updateDebtItem = async (debtId, itemId, itemUpdates) => {
                               $cond: [
                                 { $eq: ["$$item._id", itemObjectId] },
                                 {
-                                  $mergeObjects: [
-                                    "$$item",
-                                    itemUpdates,
-                                  ],
+                                  $mergeObjects: ["$$item", itemUpdates],
                                 },
                                 "$$item",
                               ],
@@ -100,5 +97,96 @@ export const updateDebtItem = async (debtId, itemId, itemUpdates) => {
     },
   );
 
-  return updatedAccount?.debts?.find((debt) => debt._id.equals(debtObjectId)) ?? null;
+  return (
+    updatedAccount?.debts?.find((debt) => debt._id.equals(debtObjectId)) ?? null
+  );
+};
+
+export const deleteDebtItem = async (debtId, itemId) => {
+  const debtObjectId = new mongoose.Types.ObjectId(debtId);
+  const itemObjectId = new mongoose.Types.ObjectId(itemId);
+
+  const updateAccount = await Account.findOneAndUpdate(
+    {
+      "debts._id": debtObjectId,
+      "debts.items._id": itemObjectId,
+    },
+    [
+      {
+        $set: {
+          debts: {
+            $map: {
+              input: "$debts",
+              as: "debt",
+              in: {
+                $cond: [
+                  { $eq: ["$$debt._id", debtObjectId] },
+                  {
+                    $mergeObjects: [
+                      "$$debt",
+                      {
+                        items: {
+                          $filter: {
+                            input: "$$debt.items",
+                            as: "item",
+                            cond: { $ne: ["$$item._id", itemObjectId] },
+                          },
+                        },
+                      },
+                    ],
+                  },
+                  "$$debt",
+                ],
+              },
+            },
+          },
+        },
+      },
+      {
+        $set: {
+          debts: {
+            $map: {
+              input: "$debts",
+              as: "debt",
+              in: {
+                $cond: [
+                  { $eq: ["$$debt._id", debtObjectId] },
+                  {
+                    $mergeObjects: [
+                      "$$debt",
+                      {
+                        totalAmount: {
+                          $reduce: {
+                            input: "$$debt.items",
+                            initialValue: 0,
+                            in: {
+                              $add: [
+                                "$$value",
+                                {
+                                  $multiply: [
+                                    { $ifNull: ["$$this.price", 0] },
+                                    { $ifNull: ["$$this.quantity", 1] },
+                                  ],
+                                },
+                              ],
+                            },
+                          },
+                        },
+                      },
+                    ],
+                  },
+                  "$$debt",
+                ],
+              },
+            },
+          },
+        },
+      },
+    ],
+    { returnDocument: "after", runValidators: true, updatePipeline: true },
+  );
+
+  return (
+    updateAccount?.debts?.find((debt) => debt._id.equals(debtObjectId)) ?? null
+  );
 };
